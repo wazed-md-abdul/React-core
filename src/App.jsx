27ecc19e18d3962c3174a9fb951791049ea587d5
wazed-md-abdul <wazedjiad@gmail.com>
2026-03-15 21:18:45 +0800

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import Countries  from './Countries'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Countries></Countries>
+    </> 
+  )
+}
+
+export default App
---
+++ src/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>this is my country</h1>
+        </div>
+    );
+};
+
+export default Countries;
